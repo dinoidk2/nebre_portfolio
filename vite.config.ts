b236668b167c1854c07314f680c75d9ac1407ff1
dinoidk2 <oceanaviktoria@gmail.com>
2025-04-05 16:02:31 +0800

    Update vite.config.ts
    base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // Add this line for GitHub Pages deployment
+  base: "/spiderverse-palette-dreams/",
   server: {
     host: "::",
     port: 8080,
